1405c135734c70a5526b52fff9653e2fb0cce899
ezywau <fahrezym610@gmail.com>
2025-06-11 18:04:45 +0700

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,13 @@
+import HeroSection from "./components/HeroSection";
+import FormSection from "./components/FormSection";
+
+function App() {
+  return (
+    <>
+      <HeroSection />
+      <FormSection />
+    </>
+  );
+}
+
+export default App;
